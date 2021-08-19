921218fb1abbeb7003468625bc062c6b72582382
jayendra <dimebeatengreen8@gmail.com>
2021-08-19 19:56:53 +0530

    Updates

---
+++ precache-manifest.b5afc941d483ebf6a32baba51c4bb48a.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8581d8c3beab4223bc418bed6768e22",
+    "url": "/portfolio-jayendra/index.html"
+  },
+  {
+    "revision": "df57932558467e80f347",
+    "url": "/portfolio-jayendra/static/css/main.7e332e51.chunk.css"
+  },
+  {
+    "revision": "e28112e561b6c93903fc",
+    "url": "/portfolio-jayendra/static/js/2.93f7c8a5.chunk.js"
+  },
+  {
+    "revision": "117d65892edc8adcd29d4a0d57b5a80a",
+    "url": "/portfolio-jayendra/static/js/2.93f7c8a5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df57932558467e80f347",
+    "url": "/portfolio-jayendra/static/js/main.68cd6243.chunk.js"
+  },
+  {
+    "revision": "732b41bdff24e0b65642",
+    "url": "/portfolio-jayendra/static/js/runtime-main.938676b2.js"
+  },
+  {
+    "revision": "ae3f566eae9a39f8a4fac19278e7b90a",
+    "url": "/portfolio-jayendra/static/media/Screenshot 2021-08-18 194059.ae3f566e.jpg"
+  },
+  {
+    "revision": "3ce357e71a836a86691b904396a8955c",
+    "url": "/portfolio-jayendra/static/media/cta-illustration.3ce357e7.svg"
+  },
+  {
+    "revision": "e75b14a597d878e422f5a5b32ec020d5",
+    "url": "/portfolio-jayendra/static/media/feextjoaqkf41.e75b14a5.png"
+  },
+  {
+    "revision": "7ed74b9293ab048e3b1c17113a90ee82",
+    "url": "/portfolio-jayendra/static/media/first.7ed74b92.jpg"
+  },
+  {
+    "revision": "cb478921009528abb858cefa6f834f7c",
+    "url": "/portfolio-jayendra/static/media/frt.cb478921.jpg"
+  },
+  {
+    "revision": "ea6949039d6554ebf2cf109f333f5811",
+    "url": "/portfolio-jayendra/static/media/illustration-section-01.ea694903.svg"
+  },
+  {
+    "revision": "63d0555d90c053eea1a1a7469d004661",
+    "url": "/portfolio-jayendra/static/media/illustration-section-02.63d0555d.svg"
+  },
+  {
+    "revision": "df7bd3116fa765bcea495aa0d82468b9",
+    "url": "/portfolio-jayendra/static/media/spc.df7bd311.jpg"
+  },
+  {
+    "revision": "3253b59bc8bf61d71f902448ad8516d7",
+    "url": "/portfolio-jayendra/static/media/trd.3253b59b.jpg"
+  },
+  {
+    "revision": "5bcdb145b3b60dc6328e718aa1d37779",
+    "url": "/portfolio-jayendra/static/media/webscr.5bcdb145.png"
+  }
+]);
